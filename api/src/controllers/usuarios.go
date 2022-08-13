1b7a70972c951dd39047040eae1b3b1d7ee6b999
Thiago <thiagopinho05@gmail.com>
2022-08-13 19:23:45 -0300

    🎉 create user in database

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -1,10 +1,41 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/database"
+	"api/src/model"
+	"api/src/repository"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 // CriarUsuario insere um usuário no banco de dados
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando usuário"))
+	corpoRequest, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	var usuario model.Users
+	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		log.Fatal(erro)
+	}
+
+	db, erro := database.Conectar()
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	repositorio := repository.NovoRepositorioDeUsuarios(db)
+	usuarioID, erro := repositorio.Criar(usuario)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	w.Write([]byte(fmt.Sprintf("ID inserido: %d", usuarioID)))
 }
 
 // BuscarUsuarios busca todos usuários salvos no banco
