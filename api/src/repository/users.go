8d4941291528bfa8f707e1a520a8d97594e32a1e
Thiago <thiagopinho05@gmail.com>
2022-08-13 19:22:38 -0300

    🎉 create repository to make user functions

---
+++ api/src/repository/users.go
@@ -0,0 +1,38 @@
+package repository
+
+import (
+	"api/src/model"
+	"database/sql"
+)
+
+type usuarios struct {
+	db *sql.DB
+}
+
+func NovoRepositorioDeUsuarios(db *sql.DB) *usuarios {
+	return &usuarios{db}
+}
+
+// Criar insere um usuário no banco de dados
+func (repositorio usuarios) Criar(usuario model.Users) (uint64, error) {
+	statement, erro := repositorio.db.Prepare("INSERT INTO usuarios (nome, nick, email, senha) values (?, ?, ?, ?)")
+	if erro != nil {
+		return 0, erro
+	}
+	defer statement.Close()
+
+	resultado, erro := statement.Exec(usuario.Nome, usuario.Nick, usuario.Nick, usuario.Email, usuario.Senha)
+
+	if erro != nil {
+
+		return 0, erro
+	}
+
+	ultimoIDInserido, erro := resultado.LastInsertId()
+
+	if erro != nil {
+		return 0, erro
+	}
+	return uint64(ultimoIDInserido), nil
+
+}
