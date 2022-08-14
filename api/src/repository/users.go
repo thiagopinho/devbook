927a291ab86a07c30051213a9ae2ec3ef4dad557
Thiago <thiagopinho05@gmail.com>
2022-08-14 16:07:56 -0300

    fix: removing unnecessary parameter and renaming struct

--- api/src/repository/users.go
+++ api/src/repository/users.go
@@ -5,23 +5,23 @@ import (
 	"database/sql"
 )
 
-type usuarios struct {
+type Usuarios struct {
 	db *sql.DB
 }
 
-func NovoRepositorioDeUsuarios(db *sql.DB) *usuarios {
-	return &usuarios{db}
+func NovoRepositorioDeUsuarios(db *sql.DB) *Usuarios {
+	return &Usuarios{db}
 }
 
 // Criar insere um usuário no banco de dados
-func (repositorio usuarios) Criar(usuario model.Users) (uint64, error) {
+func (repositorio Usuarios) Criar(usuario model.Users) (uint64, error) {
 	statement, erro := repositorio.db.Prepare("INSERT INTO usuarios (nome, nick, email, senha) values (?, ?, ?, ?)")
 	if erro != nil {
 		return 0, erro
 	}
 	defer statement.Close()
 
-	resultado, erro := statement.Exec(usuario.Nome, usuario.Nick, usuario.Nick, usuario.Email, usuario.Senha)
+	resultado, erro := statement.Exec(usuario.Nome, usuario.Nick, usuario.Email, usuario.Senha)
 
 	if erro != nil {
 
